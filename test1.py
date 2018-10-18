2a2fd308170b87b783c24ca1469331b15c5aeb65
kakurong <86200528@qq.com>
2018-10-18 16:30:17 +0800

    UDF NOT IN SPARK2.1

--- test1.py
+++ test1.py
@@ -5,10 +5,12 @@ spark = SparkSession \
     .appName("Python Spark SQL Hive integration for clean data") \
     .enableHiveSupport() \
     .getOrCreate()
+
 database =  'service_system_db'
 table = 'exit_jour'
 limit = ' '
-sql = "select count(*) from {database}.{table} {limit}".format(database=database, table=table, limit=limit)
+sql = "select count(n_ex_date) from {database}.{table} {limit}".format(database=database, table=table, limit=limit)
+
 print(sql)
 spark.sql(sql).show()
 spark.stop()
