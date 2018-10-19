ce5c9aa7513fb760cf476887fe819fa1b08129f3
kakurong <86200528@qq.com>
2018-10-19 19:07:05 +0800

    UDF NOT IN SPARK2.1

--- test2.py
+++ test2.py
@@ -1,6 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import expr, when
-import pyspark.sql.functions as func
+
 spark = SparkSession \
     .builder \
     .appName("Python Spark SQL Hive integration for clean data") \
@@ -9,18 +8,14 @@ spark = SparkSession \
 
 database =  'service_system_db'
 table = 'exit_jour'
-limit = ' '
+limit = ' limit 10'
 sql = "select * from {database}.{table} where n_ex_date < \
  20180631 and n_ex_date >20180601 {limit}".format(database=database, table=table, limit=limit)
 
-new_column_1 = expr(
-    """IF(c_ex_license != c_card_license, 1, 0)"""
-)
-print(sql)
-df = spark.sql(sql)
-df = df.withColumn("same_license", new_column_1)
-df.select('same_license','c_card_license','c_ex_license').show()
 
-df.groupBy("c_ex_license").agg(func.sum("same_license")).show()
+print(sql)
+df = spark.sql(sql).rdd.collect()
+
 
 spark.stop()
+
